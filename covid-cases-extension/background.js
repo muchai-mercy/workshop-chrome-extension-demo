6cd9e559001549c87a334f823219de839a139f5f
Mercy Muchai <muchaimercyw@gmail.com>
2020-04-22 22:47:56 +0300

    update to get total covid cases

--- covid-cases-extension/background.js
+++ covid-cases-extension/background.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
-      message: 'covid-cases',
+      message: 'covid cases',
       data: tabs[0].url
     });
   });
@@ -9,6 +9,12 @@ chrome.browserAction.onClicked.addListener(function() {
 
 chrome.runtime.onMessage.addListener(data => {
   if (data.type === 'Stay home') {
+    chrome.notifications.create('', data.options);
+  }
+});
+
+chrome.runtime.onMessage.addListener(data => {
+  if (data.type === 'active cases') {
     chrome.notifications.create('', data.options);
   }
 });
--- covid-cases-extension/content.js
+++ covid-cases-extension/content.js
@@ -1,22 +1,45 @@
 chrome.runtime.onMessage.addListener((request) => {
-  if (request.message === 'covid-cases') {
-    fetch('https://covid-19-statistics.p.rapidapi.com/reports/total', {
-      method: 'GET',
-      headers: {
-        "X-RapidAPI-Key": "6297ce111fmsh0c306c1b013cc90p1617d6jsn81563106ae9c",
-        'Content-Type': 'application/json'
-      }
-    }).then((res) => {
-    console.log('RES', res);
+  if (request.message === 'covid cases') {
     chrome.runtime.sendMessage('', {
       type: 'Stay home',
       options: {
         title: 'STAY AT HOME 😷',
-        message: 'Stay safe and stay at home dear!',
+        message: 'Stay safe and stay at home!',
         type: 'basic',
-        iconUrl: 'icons/icon-128.png'
+        iconUrl: 'images/house-128.png'
       }
     });
-  })
+    console.log('ARE WE EVEN HERE??')
+    getCases()
   }
 });
+
+
+async function getCases() {
+  let res = await fetch("https://corona.lmao.ninja/v2/all", {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  if (res.status == 200) {
+    let json = await res.json();
+    console.log('>>>>>>>', json);
+    await chrome.runtime.sendMessage('', {
+      type: 'active cases',
+      options: {
+        title: 'TOTAL CASES 😷',
+        message: `The total cases: ${formatNumber(json.cases)} people`,
+        type: 'basic',
+        iconUrl: 'images/house-128.png'
+      }
+    });
+    return json;
+  }
+  throw new Error(res.status);
+}
+
+function formatNumber(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
